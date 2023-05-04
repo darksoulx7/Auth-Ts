add3c9a2d257caea692b29dece5430445553e12f
Vishvas Prajapati <vishvas.prajapati@volansys.com>
2023-05-04 23:30:16 +0530

    app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import config from 'config';
 import log from './utils/logger';
 import connectToDb from './utils/connect-to-db';
 import routes from './routes';
+import deserializeUser from "./middleware/deserializeUser";
 
 const app = express();
 
 app.use(express.json()) 
+app.use(deserializeUser);
 app.use(routes)
 const port = config.get('port');
 
